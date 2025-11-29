e28941acada7ed5aa23d65a2ca6d60821ffc1ccf
ojj407 <marie.pfuderer@stud.ph-ludwigsburg.de>
2025-11-29 18:15:10 +0000

    neu

---
+++ Klasse.java
@@ -0,0 +1,9 @@
+public class Tier {
+    private String art;
+    private String name;
+
+    public Tier (art, name) {
+        this.art = art
+        this.name = name
+    }
+}
---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main (String[] args) {
+        Tier Katze = new Tier(Siam, Petzi)
+    }
+    system.out.println("Das Tier Katze:" + katze)
+}
