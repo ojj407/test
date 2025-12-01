586e55a6955c7ce6cd0afc1a41fa7d754b7428bd
ojj407 <marie.pfuderer@stud.ph-ludwigsburg.de>
2025-12-01 15:38:30 +0000

    V2

--- Main.java
+++ Main.java
@@ -1,11 +1,9 @@
 public class Main{
-    public void main (String[] args) {
+    public static void main (String[] args) {
         Tier Katze = new Tier("Siam", "Mizi");
         Tier Vogel = new Tier("Pelikan", "Peli");
 
         System.out.println("Das Tier Katze: " + Katze);
- 
-        System.out.println("Vogel: " + Vogel.getArt() + Vogel.getName());
+        System.out.println("Vogel: " + Vogel.getArt() + ", " + Vogel.getName());
     }
-
 }
